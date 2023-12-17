187c22b6ba181b1b61a4b7efa92a1d79d91f0395
julie <wr0124@gmail.com>
2023-12-17 08:06:30 +0100

    feat: try to use mask/image dataset

--- improved_consistency.py
+++ improved_consistency.py
@@ -1,6 +1,9 @@
 import hashlib
 import logging
 
+from UNet import UNet
+from UNet import UNetConfig
+from torchvision.datasets.folder import default_loader
 logging.basicConfig(level=logging.INFO)
 
 import json
@@ -37,14 +40,50 @@ from visdom import Visdom
 import numpy as np
 import torchvision.utils as vutils
 
-from visdom import Visdom
-
-viz = Visdom(env="consistency_model_anime_face_lightning")
-viz.line([0.0], [0.0], win="consis_loss", opts=dict(title="loss over time"))
 
 from options import parse_opts
 
 args = parse_opts()
+
+
+viz = Visdom(env=args.env)
+viz.line([0.0], [0.0], win="consis_loss", opts=dict(title="loss over time"))
+
+
+def worker_init_fn(worker_id):
+    np.random.seed(np.random.get_state()[1][0] + worker_id)
+
+class CustomImageFolder(ImageFolder):
+    def __init__(self, root, transform=None, target_transform=None,
+                 loader=default_loader, is_valid_file=None,
+                 image_folder="img", mask_folder="bbox"):
+        super(CustomImageFolder, self).__init__(
+            root, transform=transform, target_transform=target_transform,
+            loader=loader, is_valid_file=is_valid_file
+        )
+        self.image_folder = image_folder
+        self.mask_folder = mask_folder
+
+    def __getitem__(self, index):
+        file_path, _ = self.samples[index]
+       # print(f"filename is {file_path}")
+        filename = os.path.basename(file_path)
+        image_path = os.path.join(self.root, self.image_folder, filename)
+        mask_path = os.path.join(self.root, self.mask_folder, filename)
+
+        if not os.path.exists(image_path) or not os.path.exists(mask_path):
+            raise FileNotFoundError(f"Image or mask not found for file: {filename}")
+
+        image = self.loader(image_path)
+        mask = self.loader(mask_path)
+
+        if self.transform is not None:
+            image = self.transform(image)
+            mask = self.transform(mask)
+
+        return image, mask
+
+
 # dataModule
 @dataclass
 class ImageDataModuleConfig:
@@ -71,384 +110,33 @@ class ImageDataModule(LightningDataModule):
                 T.Lambda(lambda x: (x * 2) - 1),
             ]
         )
-        self.dataset = ImageFolder(self.config.data_dir, transform=transform)
-
-    def train_dataloader(self) -> DataLoader:
-        return DataLoader(
-            self.dataset,
-            batch_size=self.config.batch_size,
-            shuffle=True,
-            num_workers=self.config.num_workers,
-            pin_memory=self.config.pin_memory,
-            persistent_workers=self.config.persistent_workers,
-        )
-
-
-# Modules
-def GroupNorm(channels: int) -> nn.GroupNorm:
-    return nn.GroupNorm(num_groups=min(32, channels // 4), num_channels=channels)
-
-
-class SelfAttention(nn.Module):
-    def __init__(
-        self,
-        in_channels: int,
-        out_channels: int,
-        n_heads: int = 8,
-        dropout: float = 0.3,
-    ) -> None:
-        super().__init__()
-
-        self.dropout = dropout
-
-        self.qkv_projection = nn.Sequential(
-            GroupNorm(in_channels),
-            nn.Conv2d(in_channels, 3 * in_channels, kernel_size=1, bias=False),
-            Rearrange("b (i h d) x y -> i b h (x y) d", i=3, h=n_heads),
-        )
-        self.output_projection = nn.Sequential(
-            Rearrange("b h l d -> b l (h d)"),
-            nn.Linear(in_channels, out_channels, bias=False),
-            Rearrange("b l d -> b d l"),
-            GroupNorm(out_channels),
-            nn.Dropout1d(dropout),
-        )
-        self.residual_projection = nn.Conv2d(in_channels, out_channels, kernel_size=1)
-
-    def forward(self, x: Tensor) -> Tensor:
-        q, k, v = self.qkv_projection(x).unbind(dim=0)
-
-        output = F.scaled_dot_product_attention(
-            q, k, v, dropout_p=self.dropout if self.training else 0.0, is_causal=False
-        )
-        output = self.output_projection(output)
-        output = rearrange(output, "b c (x y) -> b c x y", x=x.shape[-2], y=x.shape[-1])
-
-        return output + self.residual_projection(x)
-
-
-class UNetBlock(nn.Module):
-    def __init__(
-        self,
-        in_channels: int,
-        out_channels: int,
-        noise_level_channels: int,
-        dropout: float = 0.3,
-    ) -> None:
-        super().__init__()
-
-        self.input_projection = nn.Sequential(
-            GroupNorm(in_channels),
-            nn.SiLU(),
-            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding="same"),
-            nn.Dropout2d(dropout),
-        )
-        self.noise_level_projection = nn.Sequential(
-            nn.SiLU(),
-            nn.Conv2d(noise_level_channels, out_channels, kernel_size=1),
-        )
-        self.output_projection = nn.Sequential(
-            GroupNorm(out_channels),
-            nn.SiLU(),
-            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding="same"),
-            nn.Dropout2d(dropout),
-        )
-        self.residual_projection = nn.Conv2d(in_channels, out_channels, kernel_size=1)
-
-    def forward(self, x: Tensor, noise_level: Tensor) -> Tensor:
-        h = self.input_projection(x)
-        h = h + self.noise_level_projection(noise_level)
-
-        return self.output_projection(h) + self.residual_projection(x)
-
-
-class UNetBlockWithSelfAttention(nn.Module):
-    def __init__(
-        self,
-        in_channels: int,
-        out_channels: int,
-        noise_level_channels: int,
-        n_heads: int = 8,
-        dropout: float = 0.3,
-    ) -> None:
-        super().__init__()
-
-        self.unet_block = UNetBlock(
-            in_channels, out_channels, noise_level_channels, dropout
-        )
-        self.self_attention = SelfAttention(
-            out_channels, out_channels, n_heads, dropout
-        )
-
-    def forward(self, x: Tensor, noise_level: Tensor) -> Tensor:
-        return self.self_attention(self.unet_block(x, noise_level))
-
-
-class Downsample(nn.Module):
-    def __init__(self, channels: int) -> None:
-        super().__init__()
-
-        self.projection = nn.Sequential(
-            Rearrange("b c (h ph) (w pw) -> b (c ph pw) h w", ph=2, pw=2),
-            nn.Conv2d(4 * channels, channels, kernel_size=1),
-        )
-
-    def forward(self, x: Tensor) -> Tensor:
-        return self.projection(x)
-
-
-class Upsample(nn.Module):
-    def __init__(self, channels: int) -> None:
-        super().__init__()
-
-        self.projection = nn.Sequential(
-            nn.Upsample(scale_factor=2.0, mode="nearest"),
-            nn.Conv2d(channels, channels, kernel_size=3, padding="same"),
-        )
-
-    def forward(self, x: Tensor) -> Tensor:
-        return self.projection(x)
-
-
-class NoiseLevelEmbedding(nn.Module):
-    def __init__(self, channels: int, scale: float = 0.02) -> None:
-        super().__init__()
-
-        self.W = nn.Parameter(torch.randn(channels // 2) * scale, requires_grad=False)
-
-        self.projection = nn.Sequential(
-            nn.Linear(channels, 4 * channels),
-            nn.SiLU(),
-            nn.Linear(4 * channels, channels),
-            Rearrange("b c -> b c () ()"),
-        )
-
-    def forward(self, x: Tensor) -> Tensor:
-        h = x[:, None] * self.W[None, :] * 2 * torch.pi
-        h = torch.cat([torch.sin(h), torch.cos(h)], dim=-1)
-
-        return self.projection(h)
-
-
-# Unet
-@dataclass
-class UNetConfig:
-    channels: int = 3
-    noise_level_channels: int = 256
-    noise_level_scale: float = 0.02
-    n_heads: int = 8
-    top_blocks_channels: Tuple[int, ...] = (128, 128)
-    top_blocks_n_blocks_per_resolution: Tuple[int, ...] = (2, 2)
-    top_blocks_has_resampling: Tuple[bool, ...] = (True, True)
-    top_blocks_dropout: Tuple[float, ...] = (0.0, 0.0)
-    mid_blocks_channels: Tuple[int, ...] = (256, 512)
-    mid_blocks_n_blocks_per_resolution: Tuple[int, ...] = (4, 4)
-    mid_blocks_has_resampling: Tuple[bool, ...] = (True, False)
-    mid_blocks_dropout: Tuple[float, ...] = (0.0, 0.3)
-
-
-class UNet(nn.Module):
-    def __init__(self, config: UNetConfig, checkpoint_dir: str = "") -> None:
-        super().__init__()
-
-        self.config = config
-        self.checkpoint_dir = checkpoint_dir
-
-        self.input_projection = nn.Conv2d(
-            config.channels,
-            config.top_blocks_channels[0],
-            kernel_size=3,
-            padding="same",
-        )
-        self.noise_level_embedding = NoiseLevelEmbedding(
-            config.noise_level_channels, config.noise_level_scale
-        )
-        self.top_encoder_blocks = self._make_encoder_blocks(
-            self.config.top_blocks_channels + self.config.mid_blocks_channels[:1],
-            self.config.top_blocks_n_blocks_per_resolution,
-            self.config.top_blocks_has_resampling,
-            self.config.top_blocks_dropout,
-            self._make_top_block,
-        )
-        self.mid_encoder_blocks = self._make_encoder_blocks(
-            self.config.mid_blocks_channels + self.config.mid_blocks_channels[-1:],
-            self.config.mid_blocks_n_blocks_per_resolution,
-            self.config.mid_blocks_has_resampling,
-            self.config.mid_blocks_dropout,
-            self._make_mid_block,
-        )
-        self.mid_decoder_blocks = self._make_decoder_blocks(
-            self.config.mid_blocks_channels + self.config.mid_blocks_channels[-1:],
-            self.config.mid_blocks_n_blocks_per_resolution,
-            self.config.mid_blocks_has_resampling,
-            self.config.mid_blocks_dropout,
-            self._make_mid_block,
-        )
-        self.top_decoder_blocks = self._make_decoder_blocks(
-            self.config.top_blocks_channels + self.config.mid_blocks_channels[:1],
-            self.config.top_blocks_n_blocks_per_resolution,
-            self.config.top_blocks_has_resampling,
-            self.config.top_blocks_dropout,
-            self._make_top_block,
-        )
-        self.output_projection = nn.Conv2d(
-            config.top_blocks_channels[0],
-            config.channels,
-            kernel_size=3,
-            padding="same",
-        )
-
-    def forward(self, x: Tensor, noise_level: Tensor) -> Tensor:
-        h = self.input_projection(x)
-        noise_level = self.noise_level_embedding(noise_level)
-
-        top_encoder_embeddings = []
-        for block in self.top_encoder_blocks:
-            if isinstance(block, UNetBlock):
-                h = block(h, noise_level)
-                top_encoder_embeddings.append(h)
-            else:
-                h = block(h)
-
-        mid_encoder_embeddings = []
-        for block in self.mid_encoder_blocks:
-            if isinstance(block, UNetBlockWithSelfAttention):
-                h = block(h, noise_level)
-                mid_encoder_embeddings.append(h)
-            else:
-                h = block(h)
-
-        for block in self.mid_decoder_blocks:
-            if isinstance(block, UNetBlockWithSelfAttention):
-                h = torch.cat((h, mid_encoder_embeddings.pop()), dim=1)
-                h = block(h, noise_level)
-            else:
-                h = block(h)
-
-        for block in self.top_decoder_blocks:
-            if isinstance(block, UNetBlock):
-                h = torch.cat((h, top_encoder_embeddings.pop()), dim=1)
-                h = block(h, noise_level)
-            else:
-                h = block(h)
-
-        return self.output_projection(h)
-
-    def _make_encoder_blocks(
-        self,
-        channels: Tuple[int, ...],
-        n_blocks_per_resolution: Tuple[int, ...],
-        has_resampling: Tuple[bool, ...],
-        dropout: Tuple[float, ...],
-        block_fn: Callable[[], nn.Module],
-    ) -> nn.ModuleList:
-        blocks = nn.ModuleList()
-
-        channel_pairs = list(zip(channels[:-1], channels[1:]))
-        for idx, (in_channels, out_channels) in enumerate(channel_pairs):
-            for _ in range(n_blocks_per_resolution[idx]):
-                blocks.append(block_fn(in_channels, out_channels, dropout[idx]))
-                in_channels = out_channels
-
-            if has_resampling[idx]:
-                blocks.append(Downsample(out_channels))
-
-        return blocks
-
-    def _make_decoder_blocks(
-        self,
-        channels: Tuple[int, ...],
-        n_blocks_per_resolution: Tuple[int, ...],
-        has_resampling: Tuple[bool, ...],
-        dropout: Tuple[float, ...],
-        block_fn: Callable[[], nn.Module],
-    ) -> nn.ModuleList:
-        blocks = nn.ModuleList()
-
-        channel_pairs = list(zip(channels[:-1], channels[1:]))[::-1]
-        for idx, (out_channels, in_channels) in enumerate(channel_pairs):
-            if has_resampling[::-1][idx]:
-                blocks.append(Upsample(in_channels))
-
-            inner_blocks = []
-            for _ in range(n_blocks_per_resolution[::-1][idx]):
-                inner_blocks.append(
-                    block_fn(in_channels * 2, out_channels, dropout[::-1][idx])
+        self.dataset = CustomImageFolder(
+                self.config.data_dir,
+                transform=transform,
+                image_folder=args.data_dir+"/img/",  # Specify the folder for images
+                mask_folder=args.data_dir+"/bbox/" ,    # Specify the folder for masks
                 )
-                out_channels = in_channels
-            blocks.extend(inner_blocks[::-1])
 
-        return blocks
-
-    def _make_top_block(
-        self, in_channels: int, out_channels: int, dropout: float
-    ) -> UNetBlock:
-        return UNetBlock(
-            in_channels,
-            out_channels,
-            self.config.noise_level_channels,
-            dropout,
-        )
-
-    def _make_mid_block(
-        self,
-        in_channels: int,
-        out_channels: int,
-        dropout: float,
-    ) -> UNetBlockWithSelfAttention:
-        return UNetBlockWithSelfAttention(
-            in_channels,
-            out_channels,
-            self.config.noise_level_channels,
-            self.config.n_heads,
-            dropout,
-        )
-
-    def save_pretrained(self, full_model_path: str):
-        # Extract the directory from the full model path
-        model_dir = os.path.dirname(full_model_path)
-
-        # Ensure the directory exists
-        if not os.path.exists(model_dir):
-            os.makedirs(model_dir, exist_ok=True)
-
-        torch.save(self.state_dict(), full_model_path)
-
-        # Save the configuration alongside the model
-        config_path = os.path.join(model_dir, "config.json")
-        with open(config_path, "w") as f:
-            json.dump(asdict(self.config), f)
-
-    @classmethod
-    def from_pretrained(cls, model_dir: str) -> "UNet":
-        # Load the configuration file
-        config_path = os.path.join(model_dir, "config.json")
-        with open(config_path, "r") as config_file:
-            config_dict = json.load(config_file)
-        config = UNetConfig(**config_dict)  # Create the config object
-
-        # Instantiate the model with the loaded configuration
-        model = cls(config)
-
-        # Load the model's state dictionary
-        model_path = os.path.join(model_dir, "final_model.ckpt")
-        state_dict = torch.load(model_path, map_location=torch.device("cpu"))
-        model.load_state_dict(state_dict)
-        return model
-
-
-# summary(UNet(UNetConfig()), input_size=((1, 3, 32, 32), (1,)))
+        def train_dataloader(self, shuffle: bool = True) -> DataLoader:
+            return DataLoader(
+                    self.dataset,
+                    batch_size=self.config.batch_size,
+                    shuffle=shuffle,
+                    num_workers=self.config.num_workers,
+                    pin_memory=self.config.pin_memory,
+                    persistent_workers=self.config.persistent_workers,
+                    worker_init_fn=worker_init_fn
+                    )
 
 # LitUNet
 @dataclass
 class LitImprovedConsistencyModelConfig:
-    ema_decay_rate: float = 0.99993
-    lr: float = 1e-4
+    lr: float = args.lr
     betas: Tuple[float, float] = (0.9, 0.995)
-    lr_scheduler_start_factor: float = 1e-5
-    lr_scheduler_iters: int = 10_000
-    sample_every_n_steps: int = 1500
-    num_samples: int = 32
+    lr_scheduler_start_factor: float = args.lr_scheduler_start_factor
+    lr_scheduler_iters: int = args.lr_scheduler_iters
+    sample_every_n_steps: int = args.sample_every_n_steps
+    num_samples: int = args.num_samples
     sampling_sigmas: Tuple[Tuple[int, ...], ...] = (
         (80,),
         (80.0, 0.661),
--- run_model.sh
+++ run_model.sh
@@ -1,10 +1,15 @@
 python3 improved_consistency.py  \
---data_dir "/data3/juliew/datasets/anime_faces/" \
+--data_dir "/data3/juliew/datasets/noglasses2glasses_ffhq/trainB" \
 --image_size 32 32 \
 --batch_size 32 \
 --num_workers 12 \
---max_steps 100000 \
+--max_steps 10000 \
+--sample_every_n_steps 50 \
 --devices 1 \
---sample_every_n_steps 500 \
+--lr 1e-4 \
+--lr_scheduler_start_factor 1e-5 \
+--lr_scheduler_iters 10_000 \
+--num_samples 32 \
+--env "consistency_model3_lightning"
 #--train_continue
 
